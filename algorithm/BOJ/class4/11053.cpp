9c2088fdac8800d70d29c2d48af067c9600d30cc
Junyong <jypark93@kookmin.ac.kr>
2023-02-25 17:59:22 +0900

    add : class4_retry_success

--- algorithm/BOJ/class4/11053.cpp
+++ algorithm/BOJ/class4/11053.cpp
@@ -1,68 +1,56 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#define MAX(a,b) (a) > (b) ? (a) : (b)
 
 using namespace std;
 
 
-int dynamic(vector<vector<int> > &dp, vector<int>&v)
+int dynamic(vector<int> &dp, vector<int>&v)
 {
-    int max = 0;
-    for(int i = 0; i < v.size() -1 ; i++){
-        int key = v[i];
-        for(int j= i + 1; j < v.size(); j++){
-            if (key < v[j]){
-                dp[i][j] =  dp[i][j-1] + 1;
-                key = v[j];
-            }
-            else
-            {
-                key = v[j];
-                for(int k = j-1; k >= 0; k--){
-                    if (key > v[k]){
-                        dp[i][j] = dp[i][k] + 1;
-                        break;
-                    }
+    int max = 1;
+    int key = v[0];
+    for(int i = 1; i < v.size(); i++){
+        int temp = 1;
+        if (key < v[i]){
+            for(int j = i -2; j>=0; j--){
+                if(v[i] > v[j]){
+                    temp = MAX(dp[j], temp);
                 }
             }
-            if (max < dp[i][j]){
-                max = dp[i][j];
-            }
+            dp[i]=  MAX(dp[i-1]+ 1, temp + 1);
+            key = v[i];
         }
+        else{
+            for(int j = i - 2; j >= 0; j--){
+                if (v[i] > v[j]){
+                    temp = MAX(temp, dp[j] + 1);
+                }
+            }
+            dp[i]= temp ;
+            key = v[i];
+        }
+        if (max < dp[i])
+            max = dp[i];
     }
     return max;
 }
-/*
-
-20
-31 84 18 62 35 77 23 53 60 94 3 77 60 51 42 18 83 85 63 51
-answer 7 
-*/
-
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    freopen("input.txt", "r", stdin);
+    // freopen("input.txt", "r", stdin);
     
     int n = 0;
     cin >> n;
 
     vector<int>v(n, 0);
-    vector<vector<int> >dp(n, vector<int>(n, 1));
+    vector<int>dp(n, 1);
 
     for(vector<int>::iterator it = v.begin(); it != v.end(); it++)
         cin >> *it;
 
     int answer = dynamic(dp,v);
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cout << dp[i][j] << " ";
-        }
-        cout << "\n";
-    }
-
     cout << answer << "\n";
     return 0;
 }
