389e63606a000374989a8dce32df509f932d5fc6
Junyong <jypark93@kookmin.ac.kr>
2023-03-02 22:36:53 +0900

    add:algorithm-class3+

---
+++ algorithm/BOJ/class3/2630.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	freopen("input.txt", "r" ,stdin);
+
+	int n = 0;
+	cin >> n;
+
+	vector<vector<int> >map(n, vector<int>(n, 0));
+	
+	return 0;
+}
