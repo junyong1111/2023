0553535e967f79df30cf3da0fab01b6edb0cabd7
Junyong <jypark93@kookmin.ac.kr>
2023-05-01 13:31:59 +0900

    delte:midterm

--- webserver/Midterm_exam/exam/book_borrow/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'book_borrow'
-urlpatterns = [
-    path('borrow', views.book_borrow, name='borrow'),
-    path('give-back', views.book_give_back, name='give-back'),
-]
--- webserver/Midterm_exam/exam/common/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
-
-app_name = 'common'
-
-urlpatterns = [
-    path('login', LoginView.as_view(template_name='auth/login.html'), name='login'),
-    path('logout', LogoutView.as_view(), name='logout'),
-]
